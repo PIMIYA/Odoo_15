1c63a629fea7ef0c2b1085d2b56e0b7e8cc29184
chenghsienyu <chenghsienyu@aim.com>
2022-03-16 20:10:55 +0100

    add new examples

--- odoo/addons/dolimi/__manifest__.py
+++ odoo/addons/dolimi/__manifest__.py
@@ -1,27 +1,20 @@
-# -*- coding: utf-8 -*-
 {
-    'name': "Dolimi",
-
+    'name': "Dolimi Agriculture Management",
     'summary': """
         Short (1 phrase/line) summary of the module's purpose, used as
         subtitle on modules listing or apps.openerp.com""",
-
     'description': """
         Long description of module's purpose
     """,
-
     'author': "PIMIYA",
     'website': "https://github.com/PIMIYA/Odoo_15/tree/main/odoo/addons/dolimi",
-
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Inventory/Argriculture',
-    'version': '15.0.1.0',
-
+    'version': '15.0.1.0.0',
     # any module necessary for this one to work correctly
     'depends': ['base'],
-
     # always loaded
     'data': [
         'views/views.xml',
@@ -34,4 +27,5 @@
     'demo': [
         'demo/demo.xml',
     ],
+    "application": True,
 }
