30d8ff48692f9ef011d30aff703cf2987890e6d3
chenghsienyu <chenghsienyu@aim.com>
2022-03-18 23:44:02 +0100

    新增基本稻作欄位

--- odoo/addons/agriculture_app/models/agriculture_crop.py
+++ odoo/addons/agriculture_app/models/agriculture_crop.py
@@ -10,6 +10,17 @@ class Crop(models.Model):
 
     crop_name = fields.Char('Crop Name', required=True)
     crop_type = fields.Char('Crop Type', required=True)
+    crop_variety = fields.Char('Crop Variety', required=True)
+    farmer = fields.Many2one('res.partner', string='Farmer', required=True)
+    farmer_id = fields.Char('Farmer ID', required=True)
+    farmer_name = fields.Char('Farmer Name', required=True)
+    farmer_phone = fields.Char('Farmer Phone', required=True)
+    farmer_email = fields.Char('Farmer Email', required=True)
+    farmer_address = fields.Char('Farmer Address', required=True)
+    farmer_city = fields.Char('Farmer City', required=True)
+    farmer_state = fields.Char('Farmer State', required=True)
+    farmer_type = fields.Selection(
+        [('non_contract', '非契作農民'), ('contract', '契作農民')], string='Farmer Type', required=True)
     active = fields.Boolean("Active?", default=True)
 
     # value = fields.Integer()
