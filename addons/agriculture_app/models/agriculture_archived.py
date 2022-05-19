e0467fb8f7212ff1a9ec560828a86bc497460d18
seanmars <mars0222@gmail.com>
2022-05-19 12:36:59 +0800

    增加農友基本資料

--- addons/agriculture_app/models/agriculture_archived.py
+++ addons/agriculture_app/models/agriculture_archived.py
@@ -11,14 +11,39 @@ class Archived(models.Model):
     _name = 'agriculture.archived'
     _description = 'Archived'
 
+    # 農民資料
     member = fields.Many2one(
         "agriculture.member", "Member", required=True)
+    SellerName = fields.Char(
+        "SellerName", related="member.SellerName", required=True)
+    SellerId = fields.Char(
+        "SellerId", related="member.SellerId", required=True)
+    Region = fields.Char("Region", related="member.Region", required=True)
+    FarmerType = fields.Selection(
+        "FarmerType", related="member.FarmerType", required=True)
+    ContractArea = fields.Float(
+        "ContractArea", related="member.ContractArea", required=True)
+    ChishangRArea = fields.Float(
+        "ChishangRArea", related="member.ChishangRArea", required=True)
+    TGAPArea = fields.Float(
+        "TGAPArea", related="member.TGAPArea", required=True)
+    OrganicVerifyDate = fields.Date(
+        "OrganicVerifyDate", related="member.OrganicVerifyDate", required=True)
+    OrganiCertifiedArea = fields.Float(
+        "OrganiCertifiedArea", related="member.OrganiCertifiedArea", required=True)
+    NonLeasedArea = fields.Float(
+        "NonLeasedArea", related="member.NonLeasedArea", required=True)
+    ##########
+
     # 成單的序號列表
     seq_numbers = fields.One2many(
         'agriculture.crop', 'archived_id', 'Numbers of CropRecord', required=True)
+    ##########
+
     # 額外的資訊
     additional_items = fields.One2many(
         'agriculture.archived_additional_item', 'archived_id', 'Extra Items')
+    ##########
 
     @api.onchange('member')
     def _onchange_member(self):
