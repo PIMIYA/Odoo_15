111a200a54ddd07fe2be71d44b512cdbd9e20fcc
chenghsienyu <chenghsienyu@aim.com>
2022-08-19 23:11:29 +0800

    新增crops api
    只取得 
    1.SeqNumber
    2. FarmerType
    3. LastCreationTime

--- addons/agriculture_app/controllers/controllers.py
+++ addons/agriculture_app/controllers/controllers.py
@@ -5,61 +5,66 @@ import json
 
 class Dolimi(http.Controller):
 
-    # @http.route('/api/v1/agriculture/crops', type='http', auth="public", methods=['GET'], csrf=False)
-    # def crops(self):
-    #     crops = http.request.env['agriculture.crop'].sudo().search([])
-    #     data = []
-    #     for record in crops:
-    #         data.append({
-    #             'SeqNumber': record.SeqNumber,
-    #             'SellerName': record.SellerName.SellerName,
-    #             'SellerId': record.SellerName.SellerId,
-    #             'Region': record.SellerName.Region,
-    #             'AuxId': record.SellerName.AuxId,
-    #             'FarmerType': record.SellerName.FarmerType,
-    #             'FarmingMethod': record.FarmingMethod,
-    #             'CropVariety': record.CropVariety.CropVariety_name,
-    #             'CropVariety_bonus': record.CropVariety.CropVariety_bonus,
-    #             'CropStatus': record.CropStatus,
-    #             'CropType': record.CropType,
-    #             'isTGAP': record.isTGAP,
-    #             'FarmingAdaption': record.FarmingAdaption,
-    #             'BrownYield': record.BrownYield,
-    #             'HullYield': record.HullYield,
-    #             'BranYield': record.BranYield,
-    #             'PrimeYield': record.PrimeYield,
-    #             'BBRiceYield': record.BBRiceYield,
-    #             'SBRiceYield': record.SBRiceYield,
-    #             'RawHumidity': record.RawHumidity,
-    #             'BrownHumidity': record.BrownHumidity,
-    #             'RiceHumidity': record.RiceHumidity,
-    #             'BrownIntactRatio': record.BrownIntactRatio,
-    #             'BrownCrackedRatio': record.BrownCrackedRatio,
-    #             'BrownImmatureRatio': record.BrownImmatureRatio,
-    #             'BrownPestsRatio': record.BrownPestsRatio,
-    #             'BrownColoredRatio': record.BrownColoredRatio,
-    #             'BrownDeadRatio': record.BrownDeadRatio,
-    #             'TasteRating': record.TasteRating,
-    #             'Protein': record.Protein,
-    #             'BrownMoisture': record.BrownMoisture,
-    #             'BrownAmylose': record.BrownAmylose,
-    #             'VolumeWeight': record.VolumeWeight,
-    #             'CarCropWeight': record.CarCropWeight,
-    #             'CarWeight': record.CarWeight,
-    #             'HarvestYear': record.HarvestYear,
-    #             'HarvestPeriod': record.HarvestPeriod,
-    #             'LastCreationTime': record.LastCreationTime,
-    #             'CropWeight': record.CropWeight,
-    #             'StorageId': record.StorageId,
-    #             'DryerId': record.DryerId,
-    #             'StartTime': record.StartTime,
-    #             'EndTime': record.EndTime,
-    #             'stage_id': record.stage_id.state,
-    #             'FinalPrice': record.FinalPrice,
-    #             'TotalPrice': record.TotalPrice,
-    #         })
+    @http.route('/api/v1/agriculture/crops', type='http', auth="public", methods=['GET'], csrf=False)
+    def crops(self):
+        crops = http.request.env['agriculture.crop'].sudo().search([])
+        data = []
+        for record in crops:
+            data.append({
+                'SeqNumber': record.SeqNumber,
+                'FarmerType': record.SellerName.FarmerType,
+                'LastCreationTime': record.LastCreationTime,
+            })
+            # data.append({
+            #     'SeqNumber': record.SeqNumber,
+            #     'SellerName': record.SellerName.SellerName,
+            #     'SellerId': record.SellerName.SellerId,
+            #     'Region': record.SellerName.Region,
+            #     'AuxId': record.SellerName.AuxId,
+            #     'FarmerType': record.SellerName.FarmerType,
+            #     'FarmingMethod': record.FarmingMethod,
+            #     'CropVariety': record.CropVariety.CropVariety_name,
+            #     'CropVariety_bonus': record.CropVariety.CropVariety_bonus,
+            #     'CropStatus': record.CropStatus,
+            #     'CropType': record.CropType,
+            #     'isTGAP': record.isTGAP,
+            #     'FarmingAdaption': record.FarmingAdaption,
+            #     'BrownYield': record.BrownYield,
+            #     'HullYield': record.HullYield,
+            #     'BranYield': record.BranYield,
+            #     'PrimeYield': record.PrimeYield,
+            #     'BBRiceYield': record.BBRiceYield,
+            #     'SBRiceYield': record.SBRiceYield,
+            #     'RawHumidity': record.RawHumidity,
+            #     'BrownHumidity': record.BrownHumidity,
+            #     'RiceHumidity': record.RiceHumidity,
+            #     'BrownIntactRatio': record.BrownIntactRatio,
+            #     'BrownCrackedRatio': record.BrownCrackedRatio,
+            #     'BrownImmatureRatio': record.BrownImmatureRatio,
+            #     'BrownPestsRatio': record.BrownPestsRatio,
+            #     'BrownColoredRatio': record.BrownColoredRatio,
+            #     'BrownDeadRatio': record.BrownDeadRatio,
+            #     'TasteRating': record.TasteRating,
+            #     'Protein': record.Protein,
+            #     'BrownMoisture': record.BrownMoisture,
+            #     'BrownAmylose': record.BrownAmylose,
+            #     'VolumeWeight': record.VolumeWeight,
+            #     'CarCropWeight': record.CarCropWeight,
+            #     'CarWeight': record.CarWeight,
+            #     'HarvestYear': record.HarvestYear,
+            #     'HarvestPeriod': record.HarvestPeriod,
+            #     'LastCreationTime': record.LastCreationTime,
+            #     'CropWeight': record.CropWeight,
+            #     'StorageId': record.StorageId,
+            #     'DryerId': record.DryerId,
+            #     'StartTime': record.StartTime,
+            #     'EndTime': record.EndTime,
+            #     'stage_id': record.stage_id.state,
+            #     'FinalPrice': record.FinalPrice,
+            #     'TotalPrice': record.TotalPrice,
+            # })
 
-    #     return json.dumps(data, default=str)
+        return json.dumps(data, default=str)
 
     @http.route("/api/v1/test/http", auth='none', type='http', method=['GET'])
     def check_method_get(self, **kwargs):
