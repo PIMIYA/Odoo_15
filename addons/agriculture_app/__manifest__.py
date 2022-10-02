361ff8e1dd0c878904cafccde5452283c0b3ccbd
chenghsienyu <chenghsienyu@aim.com>
2022-10-02 21:13:21 +0800

    加入黑貓使用者權限設置

--- addons/agriculture_app/__manifest__.py
+++ addons/agriculture_app/__manifest__.py
@@ -7,7 +7,7 @@
         Long description of module's purpose
     """,
     "author": "PIMIYA",
-    "website": "https://github.com/PIMIYA/Odoo_15/tree/main/odoo/addons/dolimi",
+    "website": "https://github.com/PIMIYA/Odoo_15",
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
