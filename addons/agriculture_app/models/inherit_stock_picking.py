6ef6adcf1f5de28fd70b7a1cf1b469a3b1ed9415
seanmars <mars0222@gmail.com>
2023-04-09 11:56:32 +0800

    修正貨運判斷邏輯

--- addons/agriculture_app/models/inherit_stock_picking.py
+++ addons/agriculture_app/models/inherit_stock_picking.py
@@ -296,7 +296,7 @@ class Inherit_stock_picking(models.Model):
         self.ensure_one()
         # 確認為物流貨運公司
         if self.carrier_id:
-            if self.carrier_id.name == 'blackcat' or '黑貓宅急便':
+            if self.carrier_id.name == 'Blackcat' or '黑貓宅急便':
                 # 呼叫物流貨運公司
                 if self.requestBlackCat(self.move_ids_without_package):
                     return self.create_notification()
@@ -304,6 +304,8 @@ class Inherit_stock_picking(models.Model):
                 # 呼叫物流貨運公司
                 if self.requestECan(self.move_ids_without_package):
                     return self.create_notification()
+
+            return self.no_carrier_notification()
         else:
             return self.no_carrier_notification()
         return True
